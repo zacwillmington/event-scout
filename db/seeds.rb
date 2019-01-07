0c54ddccb3440a47ebd59a789957441cb80c6124
github username <github email address>
2019-01-06 22:00:07 -0800

    Add to seed file and seeded DB.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
+venues = Venue.create([{ name: 'venue one' }, { name: 'venue two' }])
+
+events = Event.create([{ name: 'event one', venue_id: venues.first}, { name: 'event two', venue_id: venues.last }])
+
+users = User.create([{ user_name: 'user one'}, { user_name: 'user two' }])
+
+ticket_classes = TicketClass.create([{ name: 'ticket one', event_id: events.first}, { name: 'ticket two', event_id: events.last}])
