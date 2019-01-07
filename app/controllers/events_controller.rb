ae1853c623a00db1abea85373983a6455d41bf34
github username <github email address>
2019-01-06 22:21:49 -0800

    create routes from models

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,7 @@
 class EventsController < ApplicationController
+
+    def index
+        @events = Event.all
+        render json: @events
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources :events
+  resources :venues
+  resources :ticket_classes
 end
