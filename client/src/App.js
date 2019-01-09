c1a0a2b5246211661d00d48d073723098aa6fa38
github username <github email address>
2019-01-09 10:59:23 -0800

    set up signin container

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SigninContainer from './containers/SigninContainer';
 
 class App extends Component {
   render() {
@@ -8,18 +9,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <SigninContainer />
       </div>
     );
   }
---
+++ client/src/containers/SigninContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SigninContainer extends Component {
+
+    render() {
+      return (
+        <div>
+            text
+        </div>
+      )
+    }
+  }
+
+export default SigninContainer;
