fd1db196e3837446615bbe21a055ad87facf7441
github username <github email address>
2019-01-08 19:21:54 -0800

    Planning component structure.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  #Trim unnecessary routes after MPV is functioning
   resources :users
   resources :events
   resources :venues
