74df54d0619c7fd07e677c314318b3440305dac8
github username <zac.willmington@gmail.com>
2019-03-21 12:06:33 -0700

    Commented out unnecessary sessions_controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,28 +3,27 @@ module Api
     module V1
         class SessionsController < ApplicationController
             before_action :authenticate_user, only: [:new, :create] 
-            ##Dont need this knock does auth
-            def create 
-                @user = User.find_by(email: params[:email])
-                if @user && @user.authenticate(params['password'])
-                    session[:id] = @user.id
-                    render json: @user, status: 201 
-                else
+        #     def create 
+        #         @user = User.find_by(email: params[:email])
+        #         if @user && @user.authenticate(params['password'])
+        #             session[:id] = @user.id
+        #             render json: @user, status: 201 
+        #         else
 
-                    if @user
-                        render json: { "errors": "Your password doesn't match." }
-                    else
-                    render :json => { "errors": "Your email isn't on record with us." }
-                    end
+        #             if @user
+        #                 render json: { "errors": "Your password doesn't match." }
+        #             else
+        #             render :json => { "errors": "Your email isn't on record with us." }
+        #             end
             
-                end
-            end
+        #         end
+        #     end
 
-            def destroy
-                @user = User.find_by(email: params['email'])
-                session[:id] = nil
-                render json: {ok: true, status: 201, 'message': 'Successfully logged out.'}
-            end
+        #     def destroy
+        #         @user = User.find_by(email: params['email'])
+        #         session[:id] = nil
+        #         render json: {ok: true, status: 201, 'message': 'Successfully logged out.'}
+        #     end
         end
 
     end
