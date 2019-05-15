c5ab9f312d6ede4a1ad15867fffde83bd15a9b14
zacwillmington <zac.willmington@gmail.com>
2019-05-14 20:49:15 -0700

    Changed base url for deployment

--- client/src/actions/eventsActions.js
+++ client/src/actions/eventsActions.js
@@ -38,8 +38,8 @@ function getUsersEventsFromEventScout(events, user){
     }
 }
 
-// const BASE_URL = 'http://event-scout.herokuapp.com/';
-const BASE_URL = 'http://localhost:5000/';
+const BASE_URL = 'http://event-scout.herokuapp.com/';
+// const BASE_URL = 'http://localhost:5000/';
 
 export const getEvents = (searchTerm, geoLocation) => {
     return dispatch => {
--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router} from 'react-router-dom';
-
 import { Provider } from 'react-redux';
 import { Provider as AlertProvider }  from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
-
 import { store, persistor } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
+
 
 const options = {
     position: 'top left',
